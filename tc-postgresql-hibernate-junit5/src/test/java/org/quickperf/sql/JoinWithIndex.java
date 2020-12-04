90bc97b3a37cf6431e873ef6aa1752b70586001a
Jean Bisutti <jean.bisutti@gmail.com>
2020-12-04 10:57:11 +0100

    Add PostgreSqlTest2

--- tc-postgresql-hibernate-junit5/src/test/java/org/quickperf/sql/JoinWithIndex.java
+++ tc-postgresql-hibernate-junit5/src/test/java/org/quickperf/sql/JoinWithIndex.java
@@ -1,5 +1,7 @@
 package org.quickperf.sql;
 
+import org.junit.After;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.quickperf.annotation.MeasureExecutionTime;
@@ -8,17 +10,18 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
-public class JoinWithIndex extends PostgreSqlTest {
+public class JoinWithIndex extends PostgreSqlTest2 {
 
-    int executionCount = 10_000;
+    int executionCount = 100;
 
-    @BeforeEach
-    public void before() throws SQLException {
+    @BeforeAll
+    public static void before() throws SQLException {
 
         int batchSize = 50;
+        System.out.println("JoinWithIndex.before");
 
         List<Long> idsTeamList = TestData.insertTeams(connection, 10_000, batchSize);
-        TestData.insertPlayers(connection, 100, idsTeamList, batchSize);
+        TestData.insertPlayers(connection, 10_000, idsTeamList, batchSize);
     }
 
     @Test
@@ -36,12 +39,6 @@ public class JoinWithIndex extends PostgreSqlTest {
     public void sql_query_without_index() throws SQLException {
 
         PreparedStatement preparedStatement;
-
-        preparedStatement = connection.prepareStatement("DROP INDEX index_team_id");
-        preparedStatement.execute();
-
-        preparedStatement = connection.prepareStatement("DROP INDEX index_player_team");
-        preparedStatement.execute();
 
         preparedStatement = connection.prepareStatement("SELECT p1_0.id, p1_0.firstName, p1_0.lastName, t1_0.id, t1_0.name" +
                 " FROM Player AS p1_0 LEFT OUTER JOIN Team AS t1_0 ON p1_0.team_id = t1_0.id");
@@ -67,18 +64,25 @@ public class JoinWithIndex extends PostgreSqlTest {
 
         PreparedStatement preparedStatement;
 
-        preparedStatement = connection.prepareStatement("DROP INDEX index_team_id");
-        preparedStatement.execute();
-
-        preparedStatement = connection.prepareStatement("DROP INDEX index_player_team");
-        preparedStatement.execute();
-
         preparedStatement = connection.prepareStatement("SELECT p1_0.id, p1_0.firstName, p1_0.lastName, t1_0.id, t1_0.name" +
                 " FROM Player AS p1_0 LEFT OUTER JOIN Team AS t1_0 ON p1_0.team_id = t1_0.id");
 
         for (int i = 0; i < executionCount; i++) {
             preparedStatement.execute();
         }
+    }
+
+    //@After
+    public void after() throws SQLException {
+
+        PreparedStatement preparedStatement;
+
+        preparedStatement = connection.prepareStatement("DROP INDEX IF EXISTS index_team_id");
+        preparedStatement.execute();
+
+        preparedStatement = connection.prepareStatement("DROP INDEX IF EXISTS index_player_team");
+        preparedStatement.execute();
+
     }
 
 }
---
+++ tc-postgresql-hibernate-junit5/src/test/java/org/quickperf/sql/PostgreSqlTest2.java
@@ -0,0 +1,58 @@
+package org.quickperf.sql;
+
+import net.ttddyy.dsproxy.support.ProxyDataSource;
+import org.hibernate.internal.SessionImpl;
+import org.junit.jupiter.api.AfterAll;
+import org.quickperf.junit5.QuickPerfTest;
+import org.quickperf.sql.config.QuickPerfSqlDataSourceBuilder;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.quickperf.sql.config.HibernateEntityManagerBuilder.anHibernateEntityManager;
+import static org.quickperf.sql.config.TestDataSourceBuilder.aDataSource;
+
+@Testcontainers
+@QuickPerfTest
+public class PostgreSqlTest2 {
+
+    static final PostgreSQLContainer db =
+            new PostgreSQLContainer<>("postgres:12.3")
+                    .withDatabaseName("testcontainers")
+                    .withUsername("nes")
+                    .withPassword("quick");
+
+    protected static final Connection connection;
+
+    protected static final EntityManager entityManager;
+
+    static {
+
+        db.start();
+
+        //db.
+        final DataSource dataSource = aDataSource().build(db);
+
+        // A data source proxy is built to allow QuickPerf to intercept the SQL
+        // statements
+        final ProxyDataSource proxyDataSource = QuickPerfSqlDataSourceBuilder
+                .aDataSourceBuilder()
+                .buildProxy(dataSource);
+
+        entityManager = anHibernateEntityManager(proxyDataSource);
+
+        SessionImpl session = (SessionImpl) entityManager.getDelegate();
+
+        connection = session.connection();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        db.stop();
+    }
+
+}
